b45a49e1473cdcc43f98bf2c6fc6f3338fa7bbd6
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-08-27 19:19:35 +0200

    added functions to read console values

--- src/main/java/de/joernneumeyer/tinyscript/library/IO.java
+++ src/main/java/de/joernneumeyer/tinyscript/library/IO.java
@@ -1,12 +1,26 @@
 package de.joernneumeyer.tinyscript.library;
 
+import java.util.Scanner;
+
 import de.joernneumeyer.tinyscript.runtime.ExecutionContext;
 
 @Library
 public class IO {
+  static Scanner consoleScanner = new Scanner(System.in);
+  
   @Parameters(names = {"text"})
   public static Object print(ExecutionContext ec) {
     System.out.println(ec.getVariableByName("text").getValue());
     return null;
+  }
+  
+  public static Object scan(ExecutionContext ec) {
+    return consoleScanner.nextLine();
+  }
+  
+  @Parameters(names = {"prompt"})
+  public static Object prompt(ExecutionContext ec) {
+    System.out.print(ec.getVariableByName("prompt").getValue());
+    return scan(ec);
   }
 }
